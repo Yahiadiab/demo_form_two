52c6ee482b4e1d691129a7490823405c9f96440f
DIAB Yahia OBS/OINIS <rfvh4352@orange.com>
2025-02-25 14:17:01 +0200

    UPDATE & DELETE FUNCTIONS

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from demoapp.forms import LogForm
 from demoapp.models import Logger
@@ -28,3 +28,22 @@ def form_view(request):
     context = {'form': form, 'saved_data': saved_data, 'show_data': show_data}
 
     return render(request, "home.html", context)
+
+
+def delete_entry(request, entry_id):
+    entry = get_object_or_404(Logger, id=entry_id)
+    entry.delete()
+    return redirect(f"{reverse('Form_Model')}?show_data=true")
+
+def update_entry(request, entry_id):
+    entry = get_object_or_404(Logger, id=entry_id)
+    if request.method == "POST":
+        form = LogForm(request.POST, instance=entry)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('Form_Model'))
+    else:
+        form = LogForm(instance=entry)
+    return render(request, "update.html", {"form": form, "entry_id": entry_id })
+
+
