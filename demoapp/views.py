f890319cba6257d1040065310568fac37d35bb2c
DIAB Yahia OBS/OINIS <rfvh4352@orange.com>
2025-03-06 12:13:25 +0200

    paginator and search

--- demoapp/views.py
+++ demoapp/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from demoapp.forms import LogForm
 from demoapp.models import Logger
+from django.core.paginator import Paginator
+from django.db.models import  Q
 # Create your views here.
 
 
+
+def testing(request):
+    return render(request, "testing.html")
+
 def form_view(request):
     form = LogForm()
-    saved_data = Logger.objects.all()
-    saved_data = []  
-    show_data = False 
-    
+    page_obj = None
+    search_query=request.GET.get('search','')
+
     #to submit data 
     if request.method == 'POST':
         form = LogForm(request.POST)
@@ -19,13 +24,23 @@ def form_view(request):
             return redirect(reverse('Form_Model'))
 
     #to show data 
-    if 'show_data' in request.GET:
-        saved_data = Logger.objects.all()
-        show_data = True
+    
+    saved_data = Logger.objects.all()
+
+    if search_query :
+        saved_data = saved_data.filter(
+            Q(first_name__icontains=search_query)|
+            Q(last_name__icontains=search_query)|
+            Q(age__icontains=search_query))
+
+    paginator = Paginator(saved_data, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+        
 
 
 
-    context = {'form': form, 'saved_data': saved_data, 'show_data': show_data}
+    context = {'form': form, 'page_obj': page_obj,'search_query': search_query}
 
     return render(request, "home.html", context)
 
@@ -34,7 +49,7 @@ def delete_entry(request, entry_id):
     entry = get_object_or_404(Logger, id=entry_id)
     entry.delete()
     log = "hi" 
-    return redirect(f"{reverse('Form_Model')}?show_data=true")
+    return redirect(reverse('Form_Model'))
 
 def update_entry(request, entry_id):
     entry = get_object_or_404(Logger, id=entry_id)
